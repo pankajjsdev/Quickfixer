f95f098e1c8d5c2ccbd3baa1922405fcd9ad341d
pankajjsdev <pankaj.js.dev@gmail.com>
2024-07-18 09:50:17 +0530

    add contact us page

--- components/_ui/common/BoxBackground.tsx
+++ components/_ui/common/BoxBackground.tsx
@@ -6,7 +6,7 @@ import { BOX_BACKGROUND_CONTEXT } from '@/services/data'
 
 function BoxBackground() {
     return (
-        <div className="h-96 relative w-full overflow-hidden bg-slate-900 flex flex-col items-center justify-center my-[6rem]">
+        <div className="hidden lg:flex h-96 relative w-full overflow-hidden bg-slate-900  flex-col items-center justify-center my-[6rem]">
             <div className="absolute inset-0 w-full h-full bg-slate-900 z-20 [mask-image:radial-gradient(transparent,white)] pointer-events-none" />
 
             <Boxes />
